95511af026297c0be624408ad2b27b40cd248408
K4rlosReyes <carlosreyesml18@gmail.com>
2025-01-30 17:59:46 +0100

    dev: removed unused packages

--- configuration.nix
+++ configuration.nix
@@ -86,14 +86,11 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
     neovim
     google-chrome
     ghostty
     wpsoffice
     git
-    devenv
     docker-compose
     lazydocker
     lazygit
